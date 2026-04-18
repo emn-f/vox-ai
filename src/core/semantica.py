ad78dd9f17bbe7f120ae56067a1d71e8f8e64c6a
emanuel <95362058+emn-f@users.noreply.github.com>
2026-04-18 18:25:32 -0300

    fix: corrige tipo de tarefa para 'RETRIEVAL_QUERY' na função semantica

--- src/core/semantica.py
+++ src/core/semantica.py
@@ -22,7 +22,7 @@ def semantica(prompt):
             model=MODELO_SEMANTICO_NOME,
             contents=prompt,
             config=types.EmbedContentConfig(
-                task_type="RETRIEVAL_DOCUMENT",
+                task_type="RETRIEVAL_QUERY",
                 output_dimensionality=TAMANHO_VETOR_SEMANTICO,
             ),
         )
