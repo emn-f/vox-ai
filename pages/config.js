7c1acfe4601b489bad46fbd29ad2d23184e89403
emanuel <95362058+emn-f@users.noreply.github.com>
2026-05-13 14:16:57 +0000

    feat: adiciona arquivo de configuração com links para recursos do projeto; adição de nova colaboradora @cfmiila na sessão equipe

---
+++ pages/config.js
@@ -0,0 +1,30 @@
+const _owner = "https://github.com/emn-f";
+const _repo  = `${_owner}/vox-ai`;
+
+const VOX_LINKS = {
+
+    // GitHub
+    "OWNER_VOX": _owner,
+    "GITHUB_REPO": _repo,
+    "CHANGELOG": `${_repo}/blob/main/CHANGELOG.md`,
+    "LICENSE": `${_repo}/blob/main/LICENSE`,
+
+    "PRIVACY_POLICY": `${_repo}/blob/main/docs/legal/PRIVACY_POLICY.md`,
+    "CODE_OF_CONDUCT": `${_repo}/blob/main/.github/CODE_OF_CONDUCT.md`,
+    "SECURITY": `${_repo}/blob/main/.github/SECURITY.md`,
+    "CONTRIBUTING": `${_repo}/blob/main/.github/CONTRIBUTING.md`,
+
+    // Deploy
+    "VOX_STREAMLIT": "https://vox-ai.streamlit.app/",
+    "HUGGING_FACE": "https://huggingface.co/spaces/emn-f/vox-ai",
+
+    // Redes Sociais
+    "INSTAGRAM": "https://www.instagram.com/projetovoxai/",
+    "LINKEDIN": "https://www.linkedin.com/company/vox-ai/",
+    "FEEDBACK": "https://forms.gle/GpG9ESkUpBTA7HoV9",
+    "LINKTREE": "https://linktr.ee/vox_ai",
+
+    // Contribuidores
+    "RODRIGO_SANTOS": "https://github.com/rodrigosantos-eng",
+    "CAMILA_FERNANDES": "https://github.com/cfmiila"
+};
